7a7ab55bbe8c85fc2034b5487411bb3f8fd53a4a
Kris Konina <kris.konina@kappados.dev>
2022-08-30 13:10:20 +0200

    _

---
+++ assets/Analytics.e85cbac5.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./BrAlert.c3881f82.js";import{a0 as a,o,x as r,w as s,h as c}from"./index.72a1e99f.js";const n={},_=c(" More data is needed to perform statistical analysis. ");function i(f,l){const e=t;return o(),r(e,{level:"warning",show:!0},{default:s(()=>[_]),_:1})}var p=a(n,[["render",i]]);export{p as default};
